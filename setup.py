6db7722a4b861664d79ef45dbb42234a6024a56c
Winrey <i@iaside.com>
2023-04-13 23:35:40 +0800

    chore: modify structure

--- setup.py
+++ setup.py
@@ -2,25 +2,6 @@
 # encoding: utf-8
 
 from setuptools import setup, find_packages
- 
-setup(
-    name="fastapi-casdoor",
-    version="0.1.2",
-    python_requires='>=3.10',
-    license="MIT Licence",
-    long_description="Integration Casdoor with FastAPI",
- 
-    url="https://github.com/winrey/python-fastapi-casdoor",
-    author="winrey",
-    author_email="i@iaside.com",
- 
-    packages=find_packages(),
-    include_package_data=True,
-    platforms="any",
-    install_requires=[
-      "fastapi[all] >= 0.95.0",
-      "python-dateutil >= 2.8.2",
-      "pyjwt >= 2.0.0",
-      "pydantic >= 1.10.0",
-    ]
-)
+
+if __name__ == '__main__':
+    setup()
